214ad920079de4835878b79be1edbafd24b9a528
Ismael <ismaelalll005@gmail.com>
2019-06-13 20:30:11 -0300

    Adicionando a função de divisão corrigir

--- projeto.c
+++ projeto.c
@@ -12,7 +12,13 @@ int sub(int a,int b){
 
 int mult(int a,int b){
 	int mult = a * b;
+	return mult;
 }
+
+int div(int a,int b){
+	int div = a / b;
+	return div;
+	}
 
 
 int main(){
@@ -20,6 +26,7 @@ int main(){
 	printf("Soma dos dois numeros: %d\n",soma(5,5));
 	printf("Subtracao dos dois numeros: %d\n",sub(5,2));
 	printf("Multiplicação dos dois numeros: %d\n",mult(5,4));
+	printf("Divisao dos dois numeros: %d\n",div(10,2));
 	
 	return 0;
 }
